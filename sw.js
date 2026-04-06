afd671a4f90a75ebdf539386eef60b1752b9286c
Umut Yenilmez <umutyenilmez@gmail.com>
2026-04-07 02:09:05 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'gigtrack-v1';
+const CACHE_NAME = 'gigtrack-v4';
 const urlsToCache = [
   './index.html',
   './logo.png',
@@ -6,6 +6,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => {
@@ -14,14 +15,22 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response; // Cached version
-        }
-        return fetch(event.request); // Network version
-      })
+    fetch(event.request).catch(() => caches.match(event.request))
   );
 });
